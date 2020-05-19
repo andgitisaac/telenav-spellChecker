4b37a4d1490de8f9626873ca98d763d31d5a51bf
Pinzhu  Qian <pinzhuqian@Pinzhus-MacBook-Pro.local>
2020-05-19 10:11:54 -0700

    adding addresschecker from branch addresschecker to master

---
+++ src/demo/home/addresschecker/__init__.py
@@ -0,0 +1,15 @@
+""" The Address Checker Module """
+from .addresschecker import AddressChecker, WordFrequency
+from .info import (
+    __author__,
+    __maintainer__,
+    __email__,
+    __license__,
+    __version__,
+    __credits__,
+    __url__,
+    __bugtrack_url__,
+)
+
+
+__all__ = ["AddressChecker", "WordFrequency"]
---
+++ src/demo/home/addresschecker/info.py
@@ -0,0 +1,11 @@
+""" Information of Address Checker """
+
+
+__author__ = ""
+__maintainer__ = ""
+__email__ = ""
+__license__ = "MIT"
+__version__ = "0.0.1"
+__credits__ = ["Peter Norvig", "Tyler Barrus"]
+__url__ = ""
+__bugtrack_url__ = "{0}/issues".format(__url__)
